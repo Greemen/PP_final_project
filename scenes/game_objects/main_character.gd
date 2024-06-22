05ca721399e8454c2a2d79f2f2d4eb9bdb9abb3c
Greemen <j_andrysek@utb.cz>
2024-06-22 11:17:34 +0200

    pause menu, cleanup
    cleanup files

--- scenes/game_objects/main_character.gd
+++ scenes/game_objects/main_character.gd
@@ -8,6 +8,8 @@ const JUMP_VELOCITY = -900.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func jump():
+	velocity.y = JUMP_VELOCITY
 
 func _physics_process(delta):
 	# Animations
