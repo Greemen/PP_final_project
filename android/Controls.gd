284ad3406edd233c3f3f35cf76b0d928cac4857d
Greemen <j_andrysek@utb.cz>
2024-06-26 14:28:20 +0200

    Android support
    Jumpy .apk added to files, different layout depending on OS it run on.

---
+++ android/Controls.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var os = OS.get_name()
+	match os:
+		"Windows":
+			print("Windows")
+			disable_and_hide_node(%Controls)
+		"macOS":
+			print("macOS")
+		"Linux", "FreeBSD", "NetBSD", "OpenBSD", "BSD":
+			print("Linux/BSD")
+		"Android":
+			print("Android")
+		"iOS":
+			print("iOS")
+		"Web":
+			print("Web")
+			
+func disable_and_hide_node(node:Node) -> void:
+	node.process_mode = 4 # = Mode: Disabled
+	node.hide()
